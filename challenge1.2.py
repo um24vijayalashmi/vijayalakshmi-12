c9590084976998f91a8c192c4194026a1fa1214d
um24vijayalashmi <147599738+um24vijayalashmi@users.noreply.github.com>
2023-10-11 14:26:08 +0530

    Add files via upload

---
+++ challenge1.2.py
@@ -0,0 +1,14 @@
+# leap yaer
+
+yaer=int(input("Enter the year:")
+
+ if( year%4==0):
+   if(year%100==0):
+     if(year%400==0):
+        print(year,"is a leap year")
+ else:
+   print (year,"is not a leap year")
+
+
+   print (2016,"is leap year")
+   print (year,"Not a leap year")
